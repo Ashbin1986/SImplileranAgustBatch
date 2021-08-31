475a2ac754885ab450d625121e08ccb43a3fb00d
ashbin.kumar01 <ashbin.kumar01@gmail.com>
2021-08-31 16:44:52 +0530

    MVC+EF+Layers

---
+++ SimpliLearn.BusinessLayer/Models/EventEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpliLearn.BusinessLayer.Models
+{
+   public class EventEntity
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public DateTime? EventDate { get; set; }
+
+    }
+}
---
+++ SimpliLearn.BusinessLayer/Models/StudentEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpliLearn.BusinessLayer.Models
+{
+   public class StudentEntity
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifyDate { get; set; }
+    }
+}
